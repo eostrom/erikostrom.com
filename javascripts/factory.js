ebf6fa1cbc2c741a8070edc0ba307226ba7031c9
Erik Ostrom <erik@echographia.com>
2011-02-26 12:25:10 -0600

    Started animation (play buttons only).

--- javascripts/factory.js
+++ javascripts/factory.js
@@ -2,20 +2,15 @@
   soundManager.url = 'sm/swf/';
 
   soundManager.onready(function() {
-    function makePause() {
-      return $('<a href="#" class="pause">pause</a>').click(function() {
-        soundscape.pause(); return false;
-      });
+    function makeControl(action) {
+      return $('<a href="#" class="' + action + ' control">' +
+               action + '</a>').click(function() {
+                 soundscape[action](); return false;
+               });
     };
 
-    function makePlay() {
-      return $('<a href="#" class="play">play</a>').click(function() {
-        soundscape.play(); return false;
-      });
-    };
-
-    var pause = makePause().hide().appendTo('body');
-    var play = makePlay().hide().appendTo('body');
+    var pause = makeControl('pause').hide().appendTo('body');
+    var play = makeControl('play').hide().appendTo('body');
 
     var soundscape =
       soundManager.createSound({
@@ -27,5 +22,45 @@
       });
 
     soundscape.play({volume: 25});
+
+    var clone = play.clone().insertAfter(play).show();
+
+    var dataKey = 'soundfactory';
+
+    function step() {
+      // TODO: pause
+      // TODO: exclude the originals
+      var movers = $('.play:visible');
+
+      movers.each(function() {
+        var mover = $(this);
+        var data = mover.data(dataKey)
+        if (!data) {
+          console.log('no data');
+          data = {
+            left: parseInt(mover.css('left')),
+            bottom: parseInt(mover.css('bottom')),
+            vx: 20,
+            vy: 0
+          };
+          mover.data(dataKey, data);
+        }
+
+        data.left += data.vx;
+        data.bottom += data.vy;
+        data.xy -= .1;
+        data.vy -= .1;
+        console.log('vy ' + data.vy + ' ' + data.bottom);
+
+        if (data.bottom < -mover.outerHeight()) {
+          mover.remove();
+          return;
+        }
+
+        mover.css({left: data.left + 'px', bottom: data.bottom + 'px'});
+      });
+    };
+
+    setInterval(step, 50);
   });
 })();
