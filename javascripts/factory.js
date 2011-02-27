f060471ff9a530edeef8280e06972da41f239f40
Erik Ostrom <erik@echographia.com>
2011-02-27 17:49:03 -0600

    Made it possible to animate different buttons differently.
    Right now the pause stepper just gets it offscreen fast, but soon -
    SOON! - it will be somehow interesting.

--- javascripts/factory.js
+++ javascripts/factory.js
@@ -30,6 +30,24 @@
 
     var dataKey = 'com.erikostrom.soundfactory';
 
+    var steppers = {
+      play: {
+        start: function(mover, data) {
+          data.vx = 20;
+          data.vy = 0;
+        },
+        step: function(mover, data) {
+          data.xy -= .1;
+          data.vy -= .08;
+        }
+      },
+
+      pause: {
+        start: function(mover, data) {data.vy = -999},
+        step: function(mover, data) {}
+      }
+    };
+
     function step() {
       if (Math.random() < (1.0/50)) {
         $(controls).filter(':visible').clone().insertAfter(pause);
@@ -40,21 +58,22 @@
       movers.each(function() {
         var mover = $(this);
         var data = mover.data(dataKey)
+
         if (!data) {
           data = {
             left: parseInt(mover.css('left')),
-            bottom: parseInt(mover.css('bottom')),
-            vx: 20,
-            vy: 0
+            bottom: parseInt(mover.css('bottom'))
           };
+          data.stepper = steppers[mover.hasClass('play') ? 'play' : 'pause'];
+          data.stepper.start(mover, data);
           mover.data(dataKey, data);
         }
 
         data.left += data.vx;
         data.bottom += data.vy;
-        data.xy -= .1;
-        data.vy -= .08;
+        data.stepper.step(mover, data);
 
+        // TODO: check other edges
         if (data.bottom < -mover.outerHeight()) {
           mover.remove();
           return;
