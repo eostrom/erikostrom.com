5ecf4579350b840581718db8e08276b6e299e759
Erik Ostrom <erik@echographia.com>
2015-05-26 18:40:22 -0500

    Added notebook blog to lifestream.
    Also experimented with a bunch of other stream sources, but they
    were all found wanting.

--- javascripts/lifestream.js
+++ javascripts/lifestream.js
@@ -4,6 +4,7 @@ $("#lifestream .events").lifestream({
     $('#lifestream .events .lifestream-tumblr').truncate({max_length: 150});
   },
   list: [
+    { service: "atom", user: "http://notebook.erikostrom.com/feed.xml" },
     { service: "tumblr", user: "erikostrom",
       template: {
         posted: '<a href="${url}">{{if image}}{{html image}}{{else}}${title}{{/if}}</a>'
@@ -14,29 +15,23 @@ $("#lifestream .events").lifestream({
         bookmarked: '&hearts; <a href="${u}">${d}</a>'
       }
     },
+    //{ service: "disqus", user: "eostrom" },
     { service: "flickr", user: "55946662@N00",
       template: {
         posted: '<a href="${link}"><img width="75" src="${$item.data.media.m}" /></a>'
       }
     },
-    { service: "forrst", user: "erikostrom",
-      template: {
-        posted: '<a href="${post_url}">${title}</a>'
-      }
-    },
     { service: "github", user: "eostrom" },
-    { service: "googlereader", user: "17157162622863173265",
-      template: {
-        starred: '&hearts; <a href="${link.href}">${title.content}</a>'
-      }
-    },
     { service: "lastfm", user: "eostrom",
       template: {
         loved: '&hearts; <a href="${url}">${name}</a> by <a href="${artist.url}">${artist.name}</a>'
       }
     },
+    //{ service: "pocket", user: "erikostrom" },
+    //{ service: "quora", user: "Erik-Ostrom" },
     { service: "stackoverflow", user: "124116" },
     { service: "twitter", user: "erikostrom" },
+    //{ service: "wikipedia", user: "eostrom" },
     { service: "youtube", user: "eostrom",
       template: {
         favorited: '&hearts; <a href="${video.player.default}" title="${video.description}">${video.title}</a>',
